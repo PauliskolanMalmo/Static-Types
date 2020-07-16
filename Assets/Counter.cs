7302f649e829a0e56a1b56dc6fb6d6bc7c2be5ed
Paul B <paul.belfrage@gmail.com>
2020-07-16 20:01:13 +0200

    1. Working with Static Types
    1. Working with Static Types

---
+++ Assets/Counter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    //When using static this can be removed
+    //private Score _score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //When using static this can be removed
+        //_score = GameObject.Find("Score Keeper").GetComponent<Score>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //add 10 to score
+            //When using static this can be removed
+            //_score.score += 10;
+
+            Score.score += 10;
+            Debug.Log("Score: " + Score.score);
+        }
+    }
+}
