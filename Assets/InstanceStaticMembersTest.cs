36eb6df5a77247c64b1107411f69d0f18ade4365
Paul B <paul.belfrage@gmail.com>
2020-07-20 11:13:58 +0200

    2. Instance Members Vs. Static Members (Interview Prep)
    2. Instance Members Vs. Static Members (Interview Prep)

--- Assets/InstanceStaticMembersTest.cs
+++ Assets/InstanceStaticMembersTest.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Item
+{
+    public string name;
+    public int itemID;
+
+    //shared across all items
+    public static int itemCount;
+
+    public Item()
+    {
+
+        itemCount++;
+    }
+}
+
 public class InstanceStaticMembersTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        Item sword = new Item();
+        Item bread = new Item();
+        Item cape = new Item();
+        Item fish = new Item();
 
+        Debug.Log("Number of items: " + Item.itemCount);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
 }
