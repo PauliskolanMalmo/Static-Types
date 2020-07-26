4820bdd9e34ca01bc351959402072ffa0bc091bd
Paul B <paul.belfrage@gmail.com>
2020-07-26 12:18:24 +0200

    3. Practical Example of Working with Static Types
    3. Practical Example of Working with Static Types

---
+++ Assets/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private UIManager ui;
+    public void OnEnable()
+    {
+        SpawnManager.enemyCount++;
+        ui = GameObject.Find("UIManager").GetComponent<UIManager>();
+        ui.UpdateEnemyCount();
+        Die();
+    }
+
+    public void OnDisable()
+    {
+        SpawnManager.enemyCount--;
+        ui.UpdateEnemyCount();
+    }
+
+    void Die()
+    {
+        Destroy(this.gameObject, Random.Range(2, 6));
+    }
+}
